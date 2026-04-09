fed76b2b9f0b78d6173753d39a4c9b4211a1a819
Jesper Paulsen <jesper@atlas.co>
2026-04-09 12:47:56 +0200

    bump to v1.5.1

--- extensions/atlas_fast_mvt.cmake
+++
@@ -1,6 +0,0 @@
-# Extension config for atlas_fast_mvt
-# This file is included by DuckDB's build system to load the extension from a custom path
-
-duckdb_extension_load(atlas_fast_mvt
-    SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/atlas_fast_mvt"
-)
